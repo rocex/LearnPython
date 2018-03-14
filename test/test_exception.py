694bac24672e666ee410ee5be1e8692fee183c9e
rocex <rocexwang@gmail.com>
2018-03-14 14:24:46 +0800

    change test_exception.py

--- test/test_exception.py
+++ test/test_exception.py
@@ -3,14 +3,22 @@ fileName = 'test1.txt'
 file = None
 
 try:
+    print('1 in try')
     file = open(fileName, 'r+')
 except Exception as ex:
-    print(ex)
+    print('2 in exception', ex)
 
-    yn = input('create file(y/n)?')
+    yn = input('3 create file(y/n)? ')
     if yn == 'y':
         file = open(fileName, 'w')
 else:
+    print('4 in else, write file content')
     file.write('test exception')
 finally:
-    file.close()
+    print('5 in finally')
+
+    if file is None:
+        print('6 not close file', file)
+    else:
+        print('7 close file')
+        file.close()
