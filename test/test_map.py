4161df41258b17c51fdb6ddc411c7c0e55ca7a49
rocex <rocexwang@gmail.com>
2018-03-15 13:59:33 +0800

    test

--- test/test_map.py
+++ test/test_map.py
@@ -1,7 +1,7 @@
 
-def funMap(x, y):
-    return (x+y)
+def funMap(x, y, z):
+    return x + y + z
 
 
-print(list(map(funMap, [1], [2])))
-print(list(map(funMap, [1, 2], [3, 4])))
+print(list(map(funMap, [1], [2], [3])))
+print(list(map(funMap, [1, 2, 3], [4, 5, 6], [7, 8, 9])))
