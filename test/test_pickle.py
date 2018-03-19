b18c200d9b2011962ca0dee1869c330dc2edabd9
rocex <rocexwang@gmail.com>
2018-03-19 14:59:33 +0800

    modify test_pickly.py

--- test/test_pickle.py
+++ test/test_pickle.py
@@ -10,15 +10,20 @@ try:
     with open(fileName, 'wb') as file1:
         pickle.dump(dict1, file1)
 except Exception as ex:
-    print('exception->', ex)
+    print('in exception->', ex)
+    print('in exception, file closed? ', file1.closed)
 finally:
+    print('in finally, file closed? ', file1.closed)
     file1.close()
 
+
 try:
-    with open(fileName, 'rb') as file1:
+    with open(fileName+'1', 'rb') as file1:
         text = pickle.load(file1)
         print('reload file-->', text)
 except Exception as ex:
-    print('exception->', ex)
+    print('in exception->', ex)
+    print('in exception, file closed? ', file1.closed)
 finally:
+    print('in finally, file closed? ', file1.closed)
     file1.close()
